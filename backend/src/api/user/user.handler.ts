b75805e5d49e3b7554f4d6b218d1bda2ee1946e9
1spyral <83098914+1spyral@users.noreply.github.com>
2025-02-24 20:00:23 -0500

    Add request validation

--- backend/src/api/user/user.handler.ts
+++ backend/src/api/user/user.handler.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from "express"
+import { z } from "zod"
 
-export async function get(req: Request, res: Response) {
+export async function get(req: Request<{}, {}, {}, getParams>, res: Response) {
+    const validate = getParamsSchema.safeParse(req.query)
+
+    if (validate.error) {
+        res.status(400).json({ error: validate.error.errors })
+        return
+    }
+
     try {
-        const { username } = req.query
+        const { username } = validate.data
         const user = { message: `Fetched user ${username}` }
         res.status(200).json(user)
     } catch (error: any) {
         res.status(500).json({ error: error.message })
     }
 }
+
+const getParamsSchema = z.object({
+    username: z.string()
+})
+interface getParams extends z.infer<typeof getParamsSchema> {}
 
 export default {
     get
